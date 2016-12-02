4501f5453a9b1e7d091ea7077bf11a640fc56c70
hoameng <hoameng@upenn.edu>
2016-12-01 19:17:10 -0500

    updated IEEGTRAINFEATS

--- Code/feat_freqcorr.m
+++ Code/feat_freqcorr.m
@@ -1,42 +1,30 @@
 
 function feats = feat_freqcorr(data, fs)
 
-%     %power from 1-48hz 
-%     freqRange = 1:48;
-%     P = pmtm(data,[],size(data,1),fs);
-%     feats = log10(abs(P(freqRange,:)))';
-%     feats = reshape(feats, 1,[]);
     if ~any(any(isnan(data))) 
-        P = pmtm(data,[],1:fs/2,fs);
-
-        binSize = 5; %hz
-        freqBins = 1:binSize:size(P,1);
-        Pbins = zeros(numel(freqBins),size(data,2));
-        for i = 1:numel(freqBins)-1
-            Pbins(i,:) = sum(P(freqBins(i):freqBins(i+1),:));
-        end
-
-        Pbins = log10(abs(Pbins));
-        feats = reshape(Pbins, 1,[]);
-        fc = corr(P);
+        %fft magnitudes from 1-47Hz
+        freqRange = 1:47;
+        P = pmtm(data,[],size(data,1),fs);
+        feats = log10(abs(P(freqRange,:)))';
+        feats = reshape(feats, 1,[]);
+        
+        %fft correlation + eigenvalues
+        fc = corrcoef(zscore(P)); %normalize
+        fc_eig = eig(fc);
         fc = triu(fc,1);
         fc = fc(fc~=0)';
+        
+        feats = [feats fc fc_eig'];
 
-        %correlation between channels
-        %feats = [feats triu(corrcoef(data),-1)];
-
-        %cross correlation
-        [acor, lag] = xcorr(data,round(.25*fs),'coeff');
-        [r,c] = find(acor == max(max(acor)));
-        r =r(1);
-        toadd = reshape(acor(r,:),size(data,2),[]);
-        toadd = triu(toadd,1);
-        toadd = toadd(toadd~=0)';
-
-
-        feats = [feats toadd fc];
+        %time correlation + eigenvalues
+        tc = corrcoef(zscore(data)); %normalize
+        tc_eig = eig(tc);
+        tc = triu(tc,1);
+        tc = tc(tc~=0)';
+        
+        feats = [feats tc tc_eig'];
     else
-        feats = NaN(1,880);
+        feats = NaN(1,1024);
     end
     
 end
